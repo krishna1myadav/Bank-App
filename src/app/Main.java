2d0101065e820bb14c08ca29b86f31d0dcdd0a9e
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-08 20:32:04 +0530

    adding App package

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to Console Bank!!");
+        System.out.println("""
+                1. Open Account
+                2. Deposit
+                3. Withdraw
+                4. Transfer
+                5. Account Statement
+                6. List Accounts
+                7. Search Accounts by Customer Name
+                0. Exit
+                """);
+    }
+}
