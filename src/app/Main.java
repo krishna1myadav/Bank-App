bbfcc7f98d24b6f194198eec37c6422ba0ff2f82
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-08 22:24:15 +0530

    adding new package, name domain

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,14 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Console Bank!!");
-        System.out.println("""
+        boolean running = true;
+        while(running){
+            System.out.println("""
                 1. Open Account
                 2. Deposit
                 3. Withdraw
@@ -13,5 +18,16 @@ public class Main {
                 7. Search Accounts by Customer Name
                 0. Exit
                 """);
+            System.out.println("Enter your choice: ");
+            String choice = scanner.nextLine().trim();
+            System.out.println("Your choice: " + choice);
+            System.out.println();
+
+            switch(choice){
+                case "1" -> running = false;
+
+            }
+        }
+
     }
 }
