e416a662da23ae85317e0130665664b18a9c46b7
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-12 20:37:28 +0530

    Adding some functionality

--- src/serviece/impl/BankServieceImpl.java
+++ src/serviece/impl/BankServieceImpl.java
@@ -1,19 +1,26 @@
 package serviece.impl;
 
 import domain.Account;
+import repository.AccountRepository;
 import serviece.BankServiece;
 
 import java.util.UUID;
 
 public class BankServieceImpl implements BankServiece {
+
+    private final AccountRepository accountRepository = new AccountRepository();
     @Override
     public String openAccount(String name, String email, String accountType) {
 
         String customerId = UUID.randomUUID().toString();
-        String accountNumber = UUID.randomUUID().toString();
-        Account a = new Account(accountNumber, customerId, (double) 0 , accountType );
 
-        return "";
+
+        //String accountNumber = UUID.randomUUID().toString();
+        String temp = accountRepository.findAll().size() + 1;
+        String accountNumber = String.format("AC%06d", temp);
+        Account account = new Account(accountNumber, customerId, (double) 0 , accountType );
+        accountRepository.save(account);
+        return accountNumber;
     }
 
 
